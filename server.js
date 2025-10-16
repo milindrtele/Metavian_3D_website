e344268772787585e97816dd297364c97db18015
metavian <team.metavian@gmail.com>
2025-10-16 13:18:46 +0530

    add server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const { createServer } = require("http");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(process.env.PORT || 3015, (err) => {
+    if (err) throw err;
+    console.log("> Ready on port " + (process.env.PORT || 3015));
+  });
+});
